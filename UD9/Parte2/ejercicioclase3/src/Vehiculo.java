edb54b160c3ad9ce13d83ade77adc1bc3548b6e3
unknown <yseresp156@g.educaand.es>
2025-01-27 21:11:13 +0100

    Subida de ejercicios de cloase

---
+++ UD9/Parte2/ejercicioclase3/src/Vehiculo.java
@@ -0,0 +1,18 @@
+public class Vehiculo {
+    protected double precioMercado;
+    protected int anio;
+
+    public Vehiculo(double precioMercado, int anio){
+        this.precioMercado = precioMercado;
+        this.anio = anio;
+    }
+
+    private static int totalVehiculo(){
+
+    }
+
+    public static int obtenerTotalvehiculo(int anio){
+        return anio;
+    }
+
+}
